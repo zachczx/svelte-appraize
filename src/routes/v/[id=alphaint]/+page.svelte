a3902bf058caad12ec4e81643f2b1141239d2dc6
zachczx <zixian.chen@gmail.com>
2024-05-27 00:22:14 +0800

    Update +page.svelte

--- src/routes/v/[id=alphaint]/+page.svelte
+++ src/routes/v/[id=alphaint]/+page.svelte
@@ -106,23 +106,7 @@
 			<option selected="selected">C</option>
 			<option>D</option>
 		</select>
-		<button class="btn btn-primary text-lg font-bold"
-			><svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="24"
-				height="24"
-				viewBox="0 0 24 24"
-				fill="none"
-				stroke="currentColor"
-				stroke-width="2"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				class="icon icon-tabler icons-tabler-outline icon-tabler-plus"
-				><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M12 5l0 14" /><path
-					d="M5 12l14 0"
-				/></svg
-			></button
-		>
+		<button class="btn btn-primary text-lg font-bold">Add</button>
 	</form>
 	<ol>
 		<div id="table" class="relative grid space-y-4">
