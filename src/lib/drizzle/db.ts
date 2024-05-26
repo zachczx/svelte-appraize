a87301d6ea52170b26cb994c96b093bfb61715e0
zachczx <zixian.chen@gmail.com>
2024-05-26 16:18:20 +0800

    Update db.ts

--- src/lib/drizzle/db.ts
+++ src/lib/drizzle/db.ts
@@ -1,10 +1,10 @@
-import 'dotenv/config.js';
+import { env } from '$env/static/private';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import pg from 'pg';
 import * as schema from './schema';
 
 const client = new pg.Client({
-	connectionString: process.env.CONNECTION_STRING,
+	connectionString: env.CONNECTION_STRING,
 });
 await client.connect();
 export const db = drizzle(client, { schema });
