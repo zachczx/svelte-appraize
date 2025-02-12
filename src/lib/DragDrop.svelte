f3960538641800a86d0a9ce2ba6df8f1e3313f2f
zachczx <zixian.chen@gmail.com>
2025-02-12 23:44:01 +0800

    Update DragDrop.svelte

--- src/lib/DragDrop.svelte
+++ src/lib/DragDrop.svelte
@@ -449,25 +449,7 @@
 											Edit
 										</button>
 									</li>
-									<li>
-										<button class="flex items-center gap-2">
-											<svg
-												xmlns="http://www.w3.org/2000/svg"
-												width="1.3em"
-												height="1.3em"
-												class="tabler:message-circle-plus"
-												viewBox="0 0 24 24"
-												><path
-													fill="none"
-													stroke="currentColor"
-													stroke-linecap="round"
-													stroke-linejoin="round"
-													stroke-width="2"
-													d="M12.007 19.98A9.9 9.9 0 0 1 7.7 19L3 20l1.3-3.9C1.976 12.663 2.874 8.228 6.4 5.726c3.526-2.501 8.59-2.296 11.845.48c1.992 1.7 2.93 4.04 2.747 6.34M16 19h6m-3-3v6"
-												/></svg
-											>Add Comment
-										</button>
-									</li>
+
 									<li class="">
 										<form method="POST" class="" action="?/delete" use:enhance>
 											<input type="hidden" name="delete-target" value={person.id} />
@@ -581,7 +563,7 @@
 										/>
 									</label>
 
-									<label class="input input-bordered flex w-full items-center border">
+									<label class="input input-bordered flex w-full items-center gap-4 border">
 										<svg
 											xmlns="http://www.w3.org/2000/svg"
 											width="1em"
@@ -592,7 +574,7 @@
 											stroke-width="2"
 											stroke-linecap="round"
 											stroke-linejoin="round"
-											class="icon icon-tabler icons-tabler-outline icon-tabler-message me-2 flex-none text-base-content/50"
+											class="icon icon-tabler icons-tabler-outline icon-tabler-message text-base-content/50"
 										>
 											<path stroke="none" d="M0 0h24v24H0z" fill="none" />
 											<path d="M8 9h8" />
@@ -641,7 +623,7 @@
 		>
 			<input type="hidden" name="order" bind:value={order} />
 			<input type="hidden" name="session-id" value={session.id} />
-			<button class="btn btn-success flex items-center gap-2 text-lg font-bold lg:min-w-24">Save Changes</button>
+			<button class="btn btn-primary flex items-center gap-2 text-lg font-bold lg:min-w-24">Save Changes</button>
 		</form>
 	{/if}
 </main>
