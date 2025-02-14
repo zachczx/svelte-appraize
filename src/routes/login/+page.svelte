3ac5ec194ff24187e4c0365cb37c6eb8b9d7f3e8
zachczx <zixian.chen@gmail.com>
2025-02-15 00:23:09 +0800

    Update +page.svelte

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,17 +1,35 @@
-<script>
+<script lang="ts">
+	import { browser } from '$app/environment';
 	import { useClerkContext, SignIn } from 'svelte-clerk';
 
 	// Do not destructure context or you'll lose reactivity!
 	const ctx = useClerkContext();
 	const userId = $derived(ctx.auth.userId);
+
+	let timer: number = $state(3);
+
+	setInterval(() => {
+		timer -= 1;
+	}, 1000);
+
+	if (userId) {
+		if (browser) {
+			setTimeout(() => {
+				window.location.href = '/';
+			}, 3000);
+		}
+	}
 </script>
 
 <div class="grid content-center justify-items-center">
 	{#if userId === undefined}
-		<p>Loading...</p>
+		<span class="loading loading-spinner loading-lg text-primary"></span>
 	{:else if userId === null}
 		<SignIn signUpUrl="/signup" withSignUp={true}></SignIn>
-	{:else}
-		<div>...</div>
+	{:else if userId}
+		<span class="loading loading-spinner loading-lg text-primary"></span>
+		<div class="pt-4 text-lg">
+			You're already logged in, <a href="/">redirecting you</a> in... <b>{timer}</b>
+		</div>
 	{/if}
 </div>
