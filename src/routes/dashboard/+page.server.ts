b072800049aad9a18da4e7cd15248a146f64ede6
zachczx <zixian.chen@gmail.com>
2025-02-15 13:20:31 +0800

    Adding more to dashboard view

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -1,4 +1,7 @@
+import { db } from '$lib/drizzle/db';
+import { records, sessions, users } from '$lib/drizzle/schema';
 import { redirect } from '@sveltejs/kit';
+import { eq, count } from 'drizzle-orm';
 import { clerkClient } from 'svelte-clerk/server';
 
 export const load = async ({ locals }) => {
@@ -7,8 +10,25 @@ export const load = async ({ locals }) => {
 	if (!userId) {
 		return redirect(307, '/login');
 	}
-	const user = await clerkClient.users.getUser(userId);
-	const email = user.emailAddresses[0].emailAddress;
+	const sq = db
+		.select({ session: records.session, count: count(records.session).as('count') })
+		.from(records)
+		.groupBy(records.session)
+		.as('sq');
 
-	return { user: JSON.parse(JSON.stringify(user)), email };
+	const result = await db
+		.select({
+			id: sessions.id,
+			title: sessions.title,
+			timestamp: sessions.timestamp,
+			owner: sessions.owner,
+			slug: sessions.slug,
+			count: sq.count,
+		})
+		.from(sessions)
+		.leftJoin(sq, eq(sessions.id, sq.session))
+		.where(eq(sessions.owner, userId));
+
+	console.log(result);
+	return { streamed: { result } };
 };
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,11 +1,48 @@
 <script lang="ts">
 	import { useClerkContext } from 'svelte-clerk';
+	import { CalculateDateAgo } from '$lib/utils';
 	const ctx = useClerkContext();
 	const userId = $derived(ctx.auth.userId);
 	const user = $derived(ctx.user);
-	console.log(userId);
-
 	let { data } = $props();
 </script>
 
-{data.email}
+<main class="grid content-center justify-items-center">
+	<h1 class="pb-8 font-extrabold">Your Sessions</h1>
+	<div class="grid grid-cols-4 content-center justify-items-center gap-8 px-8">
+		{#each data.streamed.result as session}
+			<a href="/session/{session.slug}" class="card w-full max-w-96 bg-base-100 shadow-xl">
+				<div class="card-body">
+					<h2>{session.title}</h2>
+					<div class="text-base-content/70">
+						{#if session.timestamp}
+							<span>{CalculateDateAgo(session.timestamp)}</span>
+						{/if}
+						{#if session.count}
+							<span class="flex gap-2">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="1.3em"
+									height="1.3em"
+									class="tabler:user"
+									viewBox="0 0 24 24"
+									><path
+										fill="none"
+										stroke="currentColor"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0-8 0M6 21v-2a4 4 0 0 1 4-4h4a4 4 0 0 1 4 4v2"
+									/></svg
+								>{session.count}
+							</span>
+						{/if}
+					</div>
+					<!-- <div class="card-actions justify-end">
+					<button class="btn btn-primary">Buy Now</button>
+				</div> -->
+				</div>
+			</a>
+		{/each}
+	</div>
+</main>
